c270a652a8509bb1ebbe878fd1d6d2a78f0c17f9
Sivuyise15 <sivuyisematwa15@gmail.com>
2025-07-17 02:29:03 +0200

    Initial Commit

---
+++ src/main/java/org/example/localcloudstorageemulatorminis3api/LocalCloudStorageEmulatorMiniS3ApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.localcloudstorageemulatorminis3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCloudStorageEmulatorMiniS3ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalCloudStorageEmulatorMiniS3ApiApplication.class, args);
+    }
+
+}
